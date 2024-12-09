d18675f44a66ad8f01d9918b24400a4818c9abdd
Ádám Lőrincz <adam7605@easv365.dk>
2024-12-09 12:36:39 +0100

    Added User and it's id to note

--- Domain/Note.cs
+++ Domain/Note.cs
@@ -3,6 +3,7 @@
 public class Note
 {
     public Guid Id { get; set; }
+    public Guid UserId { get; set; }
     public string Title { get; set; }
     public string Content { get; set; }
     public string[] Tags { get; set; }
---
+++ Domain/User.cs
@@ -0,0 +1,7 @@
+namespace Domain;
+
+public class User
+{
+    public Guid Id { get; set; }
+    // TODO: Add other properties
+}
